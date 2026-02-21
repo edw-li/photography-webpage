5844d5e3a3d86ef82ee7febe1853c03021649e4b
Edward Li <edli2000@seas.upenn.edu>
2026-02-21 02:59:23 -0800

    Initial commit: scaffold React + TypeScript photography webpage
    Set up Vite 6 + React 18 + TypeScript project with cleaned-up
    boilerplate as a foundation for a photography portfolio site.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Photography</h1>
+      <p>Welcome to my photography portfolio.</p>
+    </div>
+  )
+}
+
+export default App
